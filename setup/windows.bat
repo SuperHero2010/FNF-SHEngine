18acbe18645a8dba97f995a2b28fbe5687f9c358
SuperHero2010 <156646084+SuperHero2010@users.noreply.github.com>
2025-09-16 10:09:33 +0700

    Update windows.bat

--- setup/windows.bat
+++ setup/windows.bat
@@ -23,7 +23,6 @@ haxelib git linc_luajit https://github.com/superpowers04/linc_luajit 1906c4a96f6
 haxelib git funkin.vis https://github.com/FunkinCrew/funkVis 22b1ce089dd924f15cdc4632397ef3504d464e90
 haxelib git grig.audio https://gitlab.com/haxe-grig/grig.audio.git cbf91e2180fd2e374924fe74844086aab7891666
 haxelib git away3d https://github.com/moxie-coder/away3d
-haxelib git hxcpp https://github.com/HaxeFoundation/hxcpp/
 haxelib git hxdiscord_rpc https://github.com/MAJigsaw77/hxdiscord_rpc
 echo Finished!
 pause
